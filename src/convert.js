51a8f534c13598085d904ac78b1bdc3b8963499c
Bruno Krebs <bruno.krebs@auth0.com>
2020-11-23 08:20:57 -0300

    converting;

--- src/convert.js
+++ src/convert.js
@@ -1,20 +1,58 @@
 const fs = require("fs");
-const xml2js = require("xml2js");
+const Saxophone = require("saxophone");
 
-const parser = new xml2js.Parser({
-  preserveChildrenOrder: true,
-});
+const parser = new Saxophone();
 
 const documentFile = __dirname + "/../docx-xml-content/word/document.xml";
+
+const dom = {
+  children: [],
+};
+const counter = 1;
+const flatDom = {};
+const stack = [dom];
+let appendText = false;
+
+const knownNodeTypes = ["document", "body", "p", "tbl", "tr", "tc"];
 
 fs.readFile(documentFile, (err, data) => {
   if (err) throw err;
 
-  parser.parseString(data, (err, result) => {
-    if (err) throw err;
-    fs.writeFile(__dirname + "/output.json", JSON.stringify(result), "utf8", (err) => {
-      if (err) throw err;
-      console.log("JSON file has been saved.");
-    });
+  parser.on("tagopen", (tag) => {
+    const nodeType = tag.name.replace("w:", "");
+
+    if (knownNodeTypes.includes(nodeType)) {
+      const newNode = {
+        type: nodeType,
+        children: [],
+      };
+      flatDom[`node${counter}`] = newNode;
+      const parent = stack[stack.length - 1];
+      parent.children.push(newNode);
+      stack.push(newNode);
+    }
+
+    if (nodeType === "t") {
+      appendText = true;
+    }
   });
+
+  parser.on("text", (text) => {
+    if (appendText) {
+      const content = text.contents.trim();
+      const parent = stack[stack.length - 1];
+      parent.content = (parent.content || "") + content;
+      appendText = false;
+    }
+  });
+
+  parser.on("tagclose", (tag) => {
+    const nodeType = tag.name.replace("w:", "");
+
+    if (knownNodeTypes.includes(nodeType)) {
+      stack.pop();
+    }
+  });
+
+  parser.parse(data);
 });
