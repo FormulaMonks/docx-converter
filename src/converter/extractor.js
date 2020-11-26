a4b797db5ffa95dbc5a4c60fa1835fd6170dd2e7
Bruno Krebs <bruno.krebs@auth0.com>
2020-11-26 17:56:32 -0300

    reorganizing;

--- src/converter/extractor.js
+++ src/converter/extractor.js
@@ -1,8 +1,21 @@
 const unzipper = require("unzipper");
+const rmfr = require("rmfr");
+const { mkdir } = require("fs");
+const { promisify } = require("util");
 
-async function extract(docxBuffer, destination) {
+const makeDir = promisify(mkdir);
+
+async function extract(docxBuffer, opts) {
+  // prepare the env to receive the extracted content
+  const extractedDocxPath = opts.extractedDocxPath || `/tmp/${Date.now()}/`;
+  if (opts.cleanUpExtractedDocxPath) {
+    await rmfr(extractedDocxPath);
+  }
+  await makeDir(extractedDocxPath);
+
+  // extract the docx files
   const zip = await unzipper.Open.buffer(docxBuffer);
-  return zip.extract({ path: destination });
+  return zip.extract({ path: extractedDocxPath });
 }
 
 module.exports = {
